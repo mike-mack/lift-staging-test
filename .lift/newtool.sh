23bc4e68c780b8a77532488b79c4e6ff22b3132b
Michael MacKinnon <michaelmackinnon@Michaels-MacBook-Pro.local>
2022-06-21 15:12:24 -0400

    new tool

---
+++ .lift/newtool.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+dir=$1
+commit=$2
+cmd=$3 
+
+function version() {
+    echo 1
+}
+
+function applicable() {
+    echo "true"
+}
+
+function run() {
+    echo "[{ \"type\": \"A but without a line number\", \
+            \"message\": \"Lift is analyzing commit $commit\", \
+            \"file\": \"file.txt\", \
+            \"category\": \"Info\", \
+            \"details_url\": \"https://example.com/#example\" \
+          }]"
+}
+
+if [[ "$cmd" = "run" ]] ; then 
+    run 
+fi 
+if [[ "$cmd" = "applicable" ]] ; then 
+    applicable 
+fi 
+if [[ "$cmd" = "version" ]] ; then 
+    version 
+fi
